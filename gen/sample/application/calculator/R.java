372cba6fa883ec8f4afdba61bd17c1fdf361ba44
ict <ict@R1-21.local>
2012-09-04 22:16:37 +0900

    commit

--- gen/sample/application/calculator/R.java
+++ gen/sample/application/calculator/R.java
@@ -50,7 +50,6 @@ public final class R {
     }
     public static final class menu {
         public static final int activity_calculator=0x7f060000;
-        public static final int menu=0x7f060001;
     }
     public static final class string {
         public static final int app_name=0x7f040000;
--- src/sample/application/calculator/CalculatorActivity.java
+++ src/sample/application/calculator/CalculatorActivity.java
@@ -20,36 +20,25 @@ import android.text.Selection;
 
 public class CalculatorActivity extends Activity {
 	
-	Integer count    = 0;
-	Double d1        = 0.0;
-	Double d2        = 0.0;
-//	String taihi     = null;
-	String enzan     = " ";
 	String strTemp   = "";
 	String strResult = "0";
 	Integer operator = 0;
-/*	ArrayList arrayList1 = new ArrayList();
-	ArrayList arrayList2 = new ArrayList();
-	ArrayList arrayList3 = new ArrayList(); */
-	Double dbk[]     = new Double[100];
-	String ebk[]     = new String[100];
-
+	
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calculator);
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.activity_calculator, menu);
-		MenuInflater mi = this.getMenuInflater();
-		mi.inflate(R.menu.menu,menu);
-        return true;
-//		return super.onCreateOptionsMenu(menu);
+        readPreferences();
     }
     
-    public void numKeyOnClick(View v){
+    @Override
+	protected void onStop() {
+		// TODO 自動生成されたメソッド・スタブ
+		super.onStop();
+        writePreferences();
+	}
+
+	public void numKeyOnClick(View v){
     	String strInKey = (String) ((Button)v).getText();
     	
     	if(strInKey.equals(".")){
@@ -62,6 +51,8 @@ public class CalculatorActivity extends Activity {
     				this.strTemp = this.strTemp + strInKey;
     			}
     		}
+    	}else{
+    		this.strTemp = this.strTemp + strInKey;
     	}
     	this.showNumber(this.strTemp);
 /*    	
@@ -155,113 +146,21 @@ public class CalculatorActivity extends Activity {
     	}
     }
     
-    public void addKeyOnClick(View v){
-     	TextView tv = (TextView) this.findViewById(R.id.displayPanel);
-		d2 = Double.parseDouble(tv.getText().toString());
-     	d1 = calculato(enzan,d1,d2);
-    	enzan = "+";
-    	tv.setText("0");
-//    	arrayList1.add(d1);
-//    	arrayList2.add(enzan);
-    	dbk[count] = d2;
-    	ebk[count] = enzan;
-    	count++;
+    public void writePreferences(){
+    	SharedPreferences prefs = getSharedPreferences("CalcPrefs", MODE_PRIVATE);
+    	SharedPreferences.Editor editor = prefs.edit();
+    	editor.putString("strTemp", strTemp);
+    	editor.putString("strResult", strResult);
+    	editor.putInt("operator", operator);
+    	editor.putString("strDisplay", ((TextView)findViewById(R.id.displayPanel)).getText().toString());
+    	editor.commit();
     }
-    public void subKeyOnClick(View v){
-     	TextView tv = (TextView) this.findViewById(R.id.displayPanel);
-		this.d2 = Double.parseDouble(tv.getText().toString());
-		this.d1 = calculato(this.enzan,this.d1,this.d2);
-		this.enzan = "-";
-    	tv.setText("0");
-    	this.dbk[count] = this.d2;
-    	this.ebk[count] = this.enzan;
-    	this.count++;
+    
+    public void readPreferences(){
+    	SharedPreferences prefs = getSharedPreferences("CalcPrefs", MODE_PRIVATE);
+    	strTemp = prefs.getString("strTemp", "");
+    	strResult = prefs.getString("strResult", "0");
+    	operator = prefs.getInt("operator", 0);
+    	((TextView)findViewById(R.id.displayPanel)).setText(prefs.getString("strDisplay", "0"));
     }
-    public void mulKeyOnClick(View v){
-     	TextView tv = (TextView) this.findViewById(R.id.displayPanel);
-		d2 = Double.parseDouble(tv.getText().toString());
-     	d1 = calculato(enzan,d1,d2);
-    	enzan = "*";
-    	tv.setText("0");
-    	dbk[count] = d2;
-    	ebk[count] = enzan;
-    	count++;
-    }
-    public void divKeyOnClick(View v){
-     	TextView tv = (TextView) this.findViewById(R.id.displayPanel);
-		d2 = Double.parseDouble(tv.getText().toString());
-     	d1 = calculato(enzan,d1,d2);
-    	enzan = "/";
-    	tv.setText("0");
-    	dbk[count] = d2;
-    	ebk[count] = enzan;
-    	count++;
-    }
-    public void sumKeyOnClick(View v){
-     	TextView tv = (TextView) this.findViewById(R.id.displayPanel);
-		d2 = Double.parseDouble(tv.getText().toString());
-     	d1 = calculato(enzan,d1,d2);
-/*    	if(enzan.equals(" ")){
-    		d1 = Double.parseDouble(tv.getText().toString());
-    	}
-    	if(enzan.equals("+")){
-    		d2 = Double.parseDouble(tv.getText().toString());
-    		d1 = d1 + d2;
-    	}
-    	if(enzan.equals("-")){
-    		d2 = Double.parseDouble(tv.getText().toString());
-    		d1 = d1 - d2;
-    	}
-    	if(enzan.equals("*")){
-    		d2 = Double.parseDouble(tv.getText().toString());
-    		d1 = d1 * d2;
-    	}
-    	if(enzan.equals("/")){
-    		d2 = Double.parseDouble(tv.getText().toString());
-    		d1 = d1 / d2;
-    	}
-*/
-    	enzan = "=";
-    	tv.setText(d1.toString());
-    	dbk[count] = d2;
-    	ebk[count] = enzan;
-    	count++;
-    	dbk[count] = d1;
-    	ebk[count] = "";
-    	count++;
-//    	for(int i = 0; i < dbk.length; i++){
-    	for(int i = 0; i < count; i++){
-         	Log.d("[dbkのtext]",dbk[i].toString());
-         	Log.d("[ebkのtext]",ebk[i]);
-    	}
-    }
-    public double calculato(String enzan,Double d1,Double d2){
-    	if(enzan.equals(" ")){
-    		d1 = d2;
-    	}
-    	if(enzan.equals("+")){
-    		d1 = d1 + d2;
-    	}
-    	if(enzan.equals("-")){
-    		d1 = d1 - d2;
-    	}
-    	if(enzan.equals("*")){
-    		d1 = d1 * d2;
-    	}
-    	if(enzan.equals("/")){
-    		d1 = d1 / d2;
-    	}
-    	return d1;
-    }
-    public void memoOnClick(View v){
-    	String str = "";
-        setContentView(R.layout.memo);
-     	EditText et = (EditText) this.findViewById(R.id.editText1);
-    	for(int i = 0; i < count; i++){
-    		str = str + dbk[i].toString() + "\n" + ebk[i].toString();
-//    		str = str + dbk[i].toString() + "¥n" + ebk[i].toString();
-    	}
-    	et.setText(str);
-    }
-
 }
